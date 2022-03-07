3cd479ddca477f092a7319f37ecdca76d4d39b04
Вахитов Рустам <therealrustam@yandex.ru>
2022-03-08 00:50:53 +0500

    Fin

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://therealrustam.sytes.net/', ]
 
 
 AUTH_USER_MODEL = 'reviews.User'
