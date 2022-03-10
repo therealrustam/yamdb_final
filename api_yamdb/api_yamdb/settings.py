60dce73530cbfdddfaf64cd30e4e3791ac12ad69
Вахитов Рустам <therealrustam@yandex.ru>
2022-03-10 19:19:50 +0500

    Final

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'api_yamdb/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'api_yamdb/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 ADMIN_ROLE = 'admin'
 MODERATOR_ROLE = 'moderator'
